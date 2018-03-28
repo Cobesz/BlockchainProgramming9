85e28eb2fd07ba55a874d0e7e628f51a5d5e447d
Cas van 't Wout <cas@bolboosch.nl>
2018-03-28 17:43:54 +0200

    fucking http

--- blockchain.js
+++ blockchain.js
@@ -1,3 +1,32 @@
+const request = require('request');
+
+let allBlocks;
+let nextBlock;
+
+function getAllBlocks() {
+    request.get("http://programmeren9.cmgt.hr.nl:8000/api/blockchain", (error, response, body) => {
+        allBlocks = JSON.parse(body);
+        // console.log(allBlocks);
+
+    });
+}
+
+
+function getNextBlock() {
+    request.get("http://programmeren9.cmgt.hr.nl:8000/api/blockchain/next", (error, response, body) => {
+        nextBlock = JSON.parse(body);
+        // console.log(nextBlock.transactions);
+
+        convertStringToAscii('met cijfer 2 erin');
+        // console.log(response);
+    });
+}
+
+getNextBlock();
+getAllBlocks();
+
+
+
 function convertStringToAscii(input) {
 
     // remove all spaces and place it in an array
@@ -9,14 +38,15 @@ function convertStringToAscii(input) {
     //loop through original array
     for (let i = 0; i < stringArray.length; i++) {
         if (!isNaN(stringArray[i])) {
-            charArray.push(Number(stringArray[i]));
+            charArray.push(stringArray[i]);
         } else {
             for (let item of stringArray[i].charCodeAt(0).toString().split('')) {
                 charArray.push(item);
             }
         }
     }
-    console.log(charArray);
+    // console.log(charArray);
 }
 
-convertStringToAscii('met cijfer 2 erin');
+
+
